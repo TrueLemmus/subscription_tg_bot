6a0849e3e235a7f6d1abdc03d98c6ad307c66c5f
TrueLemmus <sarekd@gmail.com>
2025-01-06 10:17:03 +0300

    delete old handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,12 +3,9 @@ from .buy import buy_handler
 from .pre_checkout import pre_checkout_query_handler
 from .successful_payment import successful_payment_handler
 from .help import command_help_handler
-from .payment import payment_handler
 from .callback import (plan_callback_handler,
                        move_to_payment_callback,
                        payment_method_callback,
                        telegram_wallet_payment_callback,
                        sbp_payment_callback,
-                       payed_callback,
-                       buy_callback,
                        )
--- handlers/callback/__init__.py
+++ handlers/callback/__init__.py
@@ -3,5 +3,3 @@ from .move_to_payment import move_to_payment_callback
 from .payment_method import payment_method_callback
 from .spb_payment import sbp_payment_callback
 from .telegram_wallet_payment import telegram_wallet_payment_callback
-from .payed import payed_callback
-from .buy import buy_callback
