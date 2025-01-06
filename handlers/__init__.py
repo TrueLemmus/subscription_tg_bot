3367184481b493aa3b623a6ea25548db60ccccbb
TrueLemmus <sarekd@gmail.com>
2025-01-06 10:21:28 +0300

    garbage deleted

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,7 +5,4 @@ from .successful_payment import successful_payment_handler
 from .help import command_help_handler
 from .callback import (plan_callback_handler,
                        move_to_payment_callback,
-                       payment_method_callback,
-                       telegram_wallet_payment_callback,
-                       sbp_payment_callback,
                        )
