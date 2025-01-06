3dddd8ab1ceea68ebdb496d699ef6aefe31c8992
TrueLemmus <sarekd@gmail.com>
2025-01-06 10:25:35 +0300

    removed old keyboards

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,6 +1,3 @@
 from .inline import (tariff_plan_keyboard,
                      start_handler_inline_keyboard,
-                     move_to_payment_inline_keyboard,
-                     payment_method_inline_keyboard,
-                     pay_inline_keyboard,
                      )
--- keyboards/inline/__init__.py
+++ keyboards/inline/__init__.py
@@ -1,5 +1,2 @@
 from .tariff_plan import tariff_plan_keyboard
 from .start_handler import start_handler_inline_keyboard
-from .move_to_payment import move_to_payment_inline_keyboard
-from .payment_method import payment_method_inline_keyboard
-from .pay import pay_inline_keyboard
